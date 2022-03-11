7a9781c025fae0cd808865774ef2aa1c557401f4
Jonas Kaad <36827320+JonasKaad@users.noreply.github.com>
2022-03-11 14:31:25 +0100

    Added CommonMonster interface

---
+++ CommonMonster/src/main/java/dk/sdu/mmmi/modulemon/CommonMonster/IMonster.java
@@ -0,0 +1,18 @@
+package dk.sdu.mmmi.modulemon.CommonMonster;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.ArrayList;
+
+public interface IMonster {
+
+    String getName();
+    int getHitPoints();
+    int getAttack();
+    int getDefence();
+    int getSpeed();
+    Texture getSprite();
+    MonsterType getMonsterType();
+    ArrayList<IMonsterMove> actions();
+
+}
