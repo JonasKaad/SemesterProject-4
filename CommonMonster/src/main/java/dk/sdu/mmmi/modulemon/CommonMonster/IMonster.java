881c5e5b084cc8b3ebdc3cbb4b8bb75f8a0929dd
Jonas Kaad <36827320+JonasKaad@users.noreply.github.com>
2022-03-14 10:06:37 +0100

    Update IMonster.java

--- CommonMonster/src/main/java/dk/sdu/mmmi/modulemon/CommonMonster/IMonster.java
+++ CommonMonster/src/main/java/dk/sdu/mmmi/modulemon/CommonMonster/IMonster.java
@@ -5,15 +5,13 @@ import com.badlogic.gdx.graphics.Texture;
 import java.util.List;
 
 public interface IMonster {
-
     String getName();
     int getHitPoints();
-    int setHitPoint();
+    void setHitPoints(int hitPoint);
     int getAttack();
     int getDefence();
     int getSpeed();
     Texture getSprite();
     MonsterType getMonsterType();
     List<IMonsterMove> getMoves();
-    int takeDamage();
 }
