98020b7ee62a0fb80a3f24b06d65ffaacf330ce1
Victor Boye <viboy20@student.sdu.dk>
2022-03-31 10:34:48 +0200

    Setup camera boundries for map

--- Map/src/main/java/dk/sdu/mmmi/modulemon/Map/MapView.java
+++ Map/src/main/java/dk/sdu/mmmi/modulemon/Map/MapView.java
@@ -3,10 +3,10 @@ package dk.sdu.mmmi.modulemon.Map;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import dk.sdu.mmmi.modulemon.Game;
 import dk.sdu.mmmi.modulemon.common.data.GameData;
 import dk.sdu.mmmi.modulemon.common.data.GameKeys;
@@ -15,22 +15,36 @@ import dk.sdu.mmmi.modulemon.common.services.IGameViewService;
 
 
 public class MapView extends ApplicationAdapter implements IGameViewService {
-    Texture img;
-    TiledMap tiledMap;
-    TiledMapRenderer tiledMapRenderer;
-    StretchViewport viewport;
-    OrthographicCamera cam;
+    private Texture img;
+    private TiledMap tiledMap;
+    private TiledMapRenderer tiledMapRenderer;
+    private OrthographicCamera cam;
+    private float mapLeft;
+    private float mapRight;
+    private float mapBottom;
+    private float mapTop;
 
     @Override
     public void init() {
         cam = Game.cam;
         tiledMap = new OSGiTmxLoader().load("/maps/SeasonalOverworld.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 4);
+        int scale = 4;
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, scale);
+
+        // Setting bounds for map
+        MapProperties properties = tiledMap.getProperties();
+        int mapWidth = properties.get("width", Integer.class);
+        int mapHeight = properties.get("height", Integer.class);
+        int tilePixelWidth = 16 * scale;
+        int tilePixelHeight = 16 * scale;
+        mapLeft = (cam.viewportWidth/2f);
+        mapRight = mapWidth * tilePixelWidth - (cam.viewportWidth/2f);
+        mapBottom = 360;
+        mapTop = mapBottom + mapHeight * tilePixelHeight - (cam.viewportWidth/2f) - 80;
     }
 
     @Override
     public void update(GameData gameData, IGameStateManager gameStateManager) {
-
     }
 
     @Override
@@ -41,13 +55,27 @@ public class MapView extends ApplicationAdapter implements IGameViewService {
 
     @Override
     public void handleInput(GameData gameData, IGameStateManager gameStateManager) {
-        if (gameData.getKeys().isDown(GameKeys.DOWN)) Game.cam.translate(0, -32);
-        if (gameData.getKeys().isDown(GameKeys.UP)) Game.cam.translate(0, 32);
-        if (gameData.getKeys().isDown(GameKeys.LEFT)) Game.cam.translate(-32, 0);
-        if (gameData.getKeys().isDown(GameKeys.RIGHT)) Game.cam.translate(32, 0);
-        if(gameData.getKeys().isPressed(GameKeys.ENTER)){
+        if (gameData.getKeys().isDown(GameKeys.DOWN) && cam.position.y > mapBottom) {
+            Game.cam.translate(0, -16);
+        }
+        if (gameData.getKeys().isDown(GameKeys.UP) && cam.position.y < mapTop) {
+            Game.cam.translate(0, 16);
+        }
+        if (gameData.getKeys().isDown(GameKeys.LEFT) && cam.position.x > mapLeft){
+            Game.cam.translate(-16, 0);
+        }
+        if (gameData.getKeys().isDown(GameKeys.RIGHT) && cam.position.x < mapRight){
+            Game.cam.translate(16, 0);
+        }
+        if (gameData.getKeys().isPressed(GameKeys.ENTER)){
             cam.position.set(gameData.getDisplayWidth()/2,gameData.getDisplayHeight()/2, 0);
             gameStateManager.setDefaultState();
         }
+    }
+
+    @Override
+    public void dispose() {
+        cam.position.set(cam.viewportWidth/2,cam.viewportHeight/2, 0);
+        super.dispose();
     }
 }
