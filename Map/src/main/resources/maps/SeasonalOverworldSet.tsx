d48bc95c01775b96cba765fe6340c683f853e920
Victor <zenofwolf@live.dk>
2022-04-01 10:54:34 +0200

    Updated map

--- Map/src/main/resources/maps/SeasonalOverworldSet.tsx
+++ Map/src/main/resources/maps/SeasonalOverworldSet.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="SeasonalOverworldSet" tilewidth="16" tileheight="16" tilecount="1152" columns="32">
+<tileset version="1.8" tiledversion="1.8.4" name="SeasonalOverworldSet" tilewidth="16" tileheight="16" tilecount="1152" columns="32">
  <image source="spring.png" width="512" height="576"/>
  <tile id="621" probability="0.02"/>
  <tile id="622" probability="0.01"/>
@@ -93,7 +93,6 @@
    <wangtile tileid="902" wangid="1,1,2,2,2,1,2,1"/>
    <wangtile tileid="903" wangid="1,1,2,1,2,1,2,1"/>
    <wangtile tileid="904" wangid="1,1,2,1,2,1,2,1"/>
-   <wangtile tileid="906" wangid="0,0,0,2,2,2,0,0"/>
    <wangtile tileid="929" wangid="2,2,2,1,1,1,2,2"/>
    <wangtile tileid="930" wangid="2,2,2,1,1,1,2,2"/>
    <wangtile tileid="931" wangid="2,1,2,1,1,1,2,2"/>
@@ -102,7 +101,6 @@
    <wangtile tileid="934" wangid="2,2,2,1,1,1,2,1"/>
    <wangtile tileid="935" wangid="2,1,2,1,1,1,2,1"/>
    <wangtile tileid="936" wangid="2,1,2,1,1,1,2,1"/>
-   <wangtile tileid="938" wangid="2,2,0,0,0,0,0,2"/>
    <wangtile tileid="961" wangid="2,2,2,2,2,1,1,1"/>
    <wangtile tileid="962" wangid="2,2,2,2,2,1,1,1"/>
    <wangtile tileid="963" wangid="2,1,2,2,2,1,1,1"/>
@@ -111,7 +109,6 @@
    <wangtile tileid="966" wangid="2,2,2,1,2,1,1,1"/>
    <wangtile tileid="967" wangid="2,1,2,1,2,1,1,1"/>
    <wangtile tileid="968" wangid="2,1,2,1,2,1,1,1"/>
-   <wangtile tileid="970" wangid="0,2,2,2,0,0,0,0"/>
    <wangtile tileid="993" wangid="2,1,1,1,2,2,2,2"/>
    <wangtile tileid="994" wangid="2,1,1,1,2,2,2,2"/>
    <wangtile tileid="995" wangid="2,1,1,1,2,2,2,1"/>
@@ -120,7 +117,6 @@
    <wangtile tileid="998" wangid="2,1,1,1,2,1,2,2"/>
    <wangtile tileid="999" wangid="2,1,1,1,2,1,2,1"/>
    <wangtile tileid="1000" wangid="2,1,1,1,2,1,2,1"/>
-   <wangtile tileid="1002" wangid="0,0,0,0,0,2,2,2"/>
    <wangtile tileid="1025" wangid="2,1,1,1,2,1,1,1"/>
    <wangtile tileid="1026" wangid="2,1,1,1,2,1,1,1"/>
    <wangtile tileid="1027" wangid="1,1,2,1,1,1,2,1"/>
@@ -134,16 +130,8 @@
    <wangtile tileid="1062" wangid="2,1,2,1,1,1,1,1"/>
    <wangtile tileid="1063" wangid="2,1,1,1,1,1,2,1"/>
    <wangtile tileid="1064" wangid="2,1,1,1,1,1,2,0"/>
-   <wangtile tileid="1089" wangid="0,0,2,2,2,1,1,1"/>
-   <wangtile tileid="1090" wangid="0,0,2,2,2,1,1,1"/>
-   <wangtile tileid="1091" wangid="0,0,2,2,2,2,2,0"/>
-   <wangtile tileid="1092" wangid="0,0,2,2,2,2,2,0"/>
-   <wangtile tileid="1093" wangid="0,1,1,1,2,2,2,0"/>
-   <wangtile tileid="1094" wangid="0,1,1,1,2,2,2,0"/>
    <wangtile tileid="1095" wangid="2,1,2,1,2,1,2,1"/>
    <wangtile tileid="1096" wangid="2,1,2,1,2,1,2,1"/>
-   <wangtile tileid="1098" wangid="0,2,2,2,0,0,0,0"/>
-   <wangtile tileid="1099" wangid="0,0,0,0,0,2,2,2"/>
   </wangset>
  </wangsets>
 </tileset>
