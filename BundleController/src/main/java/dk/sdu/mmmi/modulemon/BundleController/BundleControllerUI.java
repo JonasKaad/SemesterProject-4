51263394477c8d545b68b6099a6e43b07b06c23e
Alexander Nørup <me@alexandernorup.com>
2022-05-10 13:48:46 +0200

    Removed unused import

--- BundleController/src/main/java/dk/sdu/mmmi/modulemon/BundleController/BundleControllerUI.java
+++ BundleController/src/main/java/dk/sdu/mmmi/modulemon/BundleController/BundleControllerUI.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.Arrays;
