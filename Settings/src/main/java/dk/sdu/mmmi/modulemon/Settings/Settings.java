96a6d627fa74da02819e35c2f64fcb466dc3ccd8
Magnus Kjær Sørensen <magnus.kjaer310101@gmail.com>
2022-05-12 15:25:21 +0200

    Now saving settings to a json file to preserve settings after restarts

--- Settings/src/main/java/dk/sdu/mmmi/modulemon/Settings/Settings.java
+++ Settings/src/main/java/dk/sdu/mmmi/modulemon/Settings/Settings.java
@@ -2,22 +2,29 @@ package dk.sdu.mmmi.modulemon.Settings;
 
 import dk.sdu.mmmi.modulemon.common.services.IGameSettings;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Settings implements IGameSettings {
-    private Map<String, Object> settingsMap = new HashMap<>();
+
+    private SettingsHandler settingsHandler;
+
+    public Settings(){
+        try {
+            settingsHandler = new SettingsHandler("settings.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void setSetting(String setting, Object value) {
-        settingsMap.put(setting, value);
+        settingsHandler.setSetting(setting, value);
     }
 
     @Override
     public Object getSetting(String setting) {
-        if (!settingsMap.containsKey(setting)) {
-            return null;
-        }
-        return settingsMap.get(setting);
+        return settingsHandler.getSetting(setting);
     }
 }
